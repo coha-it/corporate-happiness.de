5d0408a7ee74e26f4f7d253b0222e0a97b2b5225
CoHa <it@corporate-happiness.de>
2019-04-05 11:21:50 +0200

    Neues 2019 Logo in den Header implementiert

--- wp-content/themes/produktseite-coha_tk/header.php
+++ wp-content/themes/produktseite-coha_tk/header.php
@@ -24,6 +24,7 @@
 <link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700" rel="stylesheet">-->
 
 	<?php wp_head(); ?>
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/produktseite-coha_tk/includes/css/coha-custom.css" />
 </head>
 
 <body id="body" <?php body_class(); ?> data-spy="scroll" data-target=".container-subnav" data-offset="90">
@@ -38,7 +39,9 @@
 				<div class="site-navigation-inner">
 					<div class="navbar-header">
 						<div class="mobile-logo">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/themes/produktseite-coha_tk/includes/img/coha_logo_web_250.png"/></a>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( 
+get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo esc_url( home_url( '/' ) ); 
+?>/wp-content/themes/produktseite-coha_tk/includes/img/Corporate-Happiness-Logo.svg"/></a>
 						</div>
 						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
@@ -49,7 +52,9 @@
 						</button>
 	
 						<!-- Your site title as branding in the menu -->
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/themes/produktseite-coha_tk/includes/img/coha_logo_web_250.png"></img></a>
+						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo 
+esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo esc_url( home_url( '/' ) ); 
+?>/wp-content/themes/produktseite-coha_tk/includes/img/Corporate-Happiness-Logo.svg"></img></a>
 					</div >
 
 					<!-- The WordPress Menu goes here -->
